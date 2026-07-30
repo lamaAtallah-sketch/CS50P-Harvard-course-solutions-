3abc9e6403e59bdf4609d64c7d124c21d59b94b4
lamaAtallah-sketch <lamaqwer53@gmail.com>
2026-07-30 09:58:02 +0300

    indoor.py

--- indoor.py
+++ indoor.py
@@ -1,12 +1,12 @@
-#Begin
-#ask the user to write what he wants to say 
-#turn on the whispring mood:) convert all the letters to be lowercase
-#Remove all the whitespaces from the edges
-#print the result
-#End
-whisper=input("What do u say?\n").lower().strip()
+# Begin
+# ask the user to write what he wants to say 
+# turn on the whispering  mood :) convert all the letters to lowercase
+# Remove all the whitespaces from the edges
+# print the result
+# End
+
+whisper = input("What do u say?\n").lower().strip()
 print(whisper)
-"""
-Eng.Lama Atallah 
-This is PS0 for CS50p (Indoor Voice)
-"""
+
+# Eng.Lama Atallah 
+# This is PS0 for CS50P (Indoor Voice)
