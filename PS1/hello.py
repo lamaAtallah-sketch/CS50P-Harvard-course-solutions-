9ba69372279d8be1708efee0a5ad362d502f0d9b
lamaAtallah-sketch <lamaqwer53@gmail.com>
2026-07-30 19:52:22 +0300

    Create hello.py
    Cs50p Harvard's course PS1

---
+++ PS1/hello.py
@@ -0,0 +1,14 @@
+def main():
+    name = input("What is your name?\n").strip().title()
+    greeting(name)
+
+def greeting(user):
+    print(f"hello, {user}")
+
+main()
+"""
+Eng.Lama Atallah 
+PS1
+Ask the user for their name. Then greet the user. 
+Your program should print, "hello, name" where name is the name the user inputted.
+"""
