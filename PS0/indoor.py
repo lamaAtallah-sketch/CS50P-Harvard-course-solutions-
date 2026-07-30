16321d1273a9c586f0ad970b585637446f57c143
lamaAtallah-sketch <lamaqwer53@gmail.com>
2026-07-30 20:18:26 +0300

    Create indoor.py

---
+++ PS0/indoor.py
@@ -0,0 +1,12 @@
+# Begin
+# ask the user to write what he wants to say 
+# turn on the whispering  mood :) convert all the letters to lowercase
+# Remove all the whitespaces from the edges
+# print the result
+# End
+
+whisper = input("What do u say?\n").lower().strip()
+print(whisper)
+
+# Eng.Lama Atallah 
+# This is PS0 for CS50P (Indoor Voice)
